a4b04a3afb303bbe14b6991f0d7d7c9e9558f8d5
yousef khaled <y0ussefkh1edv@gmail.com>
2025-01-21 20:57:20 +0200

    Add files via upload

---
+++ some_constants.py
@@ -0,0 +1,19 @@
+'''
+This is  a constants you will be need it to use this constants to bind event with func:
+-: func bind()
+'''
+RIGHT_CLIICK='<Button-1>'
+MID_CLICK ='<Button-2>'
+LEFT_CLICK = '<Button-3>'
+FOUCSIN_ENTRY = '<FocusIn>'
+FOUCSOUT_ENTRY = '<FocusOut>'
+CR_ENTER = '<Enter>'
+CR_LEAVE = '<Leave>'
+CHK_COMBO = '<<ComboboxSelected>>'
+CHK_LIST = '<<ListboxSelect>>'
+MOUS_WHEEL = '<MouseWheel>' 
+DUOB_CLICK = '<Double-Button-1>'
+DESTROY = '<Destroy>'
+UP ='<Up>'
+DOWN = '<Down>'
+
